3254d7ffccda2e9f9134be6fb319e5e7c01faf3d
spenguin10lui <spenguin10@yahoo.com.tw>
2021-12-28 17:50:05 +0800

    全開測試

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,19 +9,27 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://spenguinlui.github.io'
-    
-    resource '*',
-    headers: :any,
-    methods: [:get]
-    # credentials: true
-  end
-  allow do
-    origins 'localhost:8080'
+    origins '*'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
+  # allow do
+  #   origins 'https://spenguinlui.github.io'
+    
+  #   resource '*',
+  #   headers: :any,
+  #   methods: [:get]
+  #   # credentials: true
+  # end
+  # allow do
+  #   origins 'localhost:8080'
+
+  #   resource '*',
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
+  #     credentials: true
+  # end
 end
