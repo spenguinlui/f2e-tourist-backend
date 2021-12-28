f842688ff9dd922ac92396931fad1fbc136a1081
spenguin10lui <spenguin10@yahoo.com.tw>
2021-12-28 17:37:00 +0800

    修正 cors 設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,12 +9,12 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://spenguinlui.github.io/'
+    origins 'https://spenguinlui.github.io'
     
     resource '*',
     headers: :any,
-    methods: [:get, :post, :put, :patch, :delete, :options, :head],
-    credentials: true
+    methods: [:get]
+    # credentials: true
   end
   allow do
     origins 'localhost:8080'
