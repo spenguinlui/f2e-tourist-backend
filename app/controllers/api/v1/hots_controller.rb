18588f271282f51ffb10d3094592e51c9dd44cbf
spenguin10lui <spenguin10@yahoo.com.tw>
2022-02-18 15:35:20 +0800

    熱門地點改用有限時間redis

--- app/controllers/api/v1/hots_controller.rb
+++ app/controllers/api/v1/hots_controller.rb
@@ -6,14 +6,36 @@ class Api::V1::HotsController < ApplicationController
       top = params[:top] || 30
       skip = params[:skip] || 0
 
-      local_item = LocalItem.order_by_hot({ skip: skip, top: top })
-      @ids = []
-      local_item.map { |item| @ids << item.ptx_data_id }
+      @ids = get_hots_by_redis
+
+      if @ids.nil?
+        @ids = []
+        local_item = LocalItem.order_by_hot({ skip: skip, top: top })
+        local_item.map { |item| @ids << item.ptx_data_id }
+        set_hots_by_redis(@ids)
+      end
 
       render :json => { message: "成功取得熱門列表", ids: @ids, status: 200 }
     rescue Exception => e
       logger.error "----- 取得熱門列表發生錯誤！！！ -> #{e}"
       render :json => { message: "發生不明錯誤", status: 500 }, :status => :bad_request
     end
+  end
+
+  private
+
+  def get_hots_by_redis
+    @redis = Redis.new
+    redis_hots = @redis.get("hot_array")
+    if redis_hots.present?
+      JSON.parse(redis_hots)
+    else
+      nil
+    end
+  end
+
+  def set_hots_by_redis(ids)
+    @redis.set("hot_array", ids)
+    @redis.expire("hot_array", (10 * 60)) # live in 10 minute
   end
 end
